2de591feb8202ceea9f96b0c185689ad12791af2
Gavin Baker <gavinb@antonym.org>
2026-01-31 14:25:51 +1100

    Create paths for storage backend on demand

--- lib/atelier/storage.ex
+++ lib/atelier/storage.ex
@@ -1,19 +1,24 @@
 defmodule Atelier.Storage do
-  @storage_root "tmp/atelier_studio"
-
-  def init_workspace(project_id) do
-    path = Path.join(@storage_root, project_id)
-    File.mkdir_p!(path)
-    path
-  end
+  # Use Path.expand to ensure we are working with an absolute path
+  # based on where the project root is.
+  @storage_root Path.expand("tmp/atelier_studio")
 
   def write_file(project_id, filename, content) do
-    path = Path.join([@storage_root, project_id, filename])
+    # 1. Define the directory for the project
+    project_dir = Path.join(@storage_root, project_id)
+
+    # 2. Ensure the directory exists (creates tmp/atelier_studio/project_id)
+    File.mkdir_p!(project_dir)
+
+    # 3. Write the file
+    path = Path.join(project_dir, filename)
     File.write!(path, content)
+
     {:ok, path}
   end
 
   def read_file(project_id, filename) do
-    Path.join([@storage_root, project_id, filename]) |> File.read()
+    Path.join([@storage_root, project_id, filename])
+    |> File.read()
   end
 end
